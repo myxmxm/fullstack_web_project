e5b53f63451aed90289d8a271c3ec217efeec752
Xiaoming Ma <73438688+myxmxm@users.noreply.github.com>
2024-04-25 03:06:52 +0300

    test

--- shell_scripts/kill_backend_on_port.sh
+++ shell_scripts/kill_backend_on_port.sh
@@ -7,7 +7,8 @@ fi
 
 port=$1
 
-pids=($(sudo ss -tulnp | awk -v port=":$port" '$5 ~ port {split($7, arr, ","); for (i in arr) {gsub(/[^0-9]/, "", arr[i]); print arr[i]}}' | sort -u | head -n 2))
+# pids=($(sudo ss -tulnp | awk -v port=":$port" '$5 ~ port {split($7, arr, ","); for (i in arr) {gsub(/[^0-9]/, "", arr[i]); print arr[i]}}' | sort -u | head -n 2))
+pids=($(ss -tulnp | grep :$port | awk '{print $7}' | grep -oP '(?<=pid=)[0-9]+'))
 
 if [ ${#pids[@]} -eq 0 ]; then
     echo "No processes found for port $port"
